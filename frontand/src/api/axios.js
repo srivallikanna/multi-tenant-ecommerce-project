8dc3de462b27e89f7a4408e71ba1b4bd9baa5885
Srivalli Kanna <srivallikanna62@gmail.com>
2026-08-31 19:46:00 +0530

    fix frontend build and backend connection

--- frontand/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: baseURL: 'https://multi-tenant-ecommerce-project-2.onrender.com/api',
-});
-
-export default api;
---
+++ frontant/src/api/axios.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://multi-tenant-ecommerce-project-2.onrender.com/api",
+});
+
+export default api;
