2c1c2303af6f0a410cd06d960ed92f98c1ed3c01
Riya Mishra <misra7060@gmail.com>
2026-09-01 21:31:52 +0530

    Add Cart, Checkout, and Orders routes

--- frontant/src/App.jsx
+++ frontant/src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
   { path: "/product/:id", element: <ProductDetails /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/checkout", element: <Checkout /> },
+  { path: "/orders", element: <Orders /> }
 ]);
 
 export default function App() {
   return <RouterProvider router={router} />;
 }
-
-export default App;
