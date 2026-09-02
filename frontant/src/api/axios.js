3841bba75bfd7374a50fa55910630bae89aeae76
Srivalli Kanna <srivallikanna62@gmail.com>
2026-09-02 21:08:50 +0530

    Connect frontend to Render backend

--- frontant/src/api/axios.js
+++ frontant/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://multi-tenant-ecommerce-project-2.onrender.com/api',
 });
 
 export const API = api;
