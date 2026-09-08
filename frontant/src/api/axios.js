037235f27efde378e675766dbf618cfa424a3b7d
Srivalli Kanna <srivallikanna62@gmail.com>
2026-09-08 18:19:46 +0530

    Update production API URL

--- frontant/src/api/axios.js
+++ frontant/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://multi-tenant-ecommerce-project-03.onrender.com/api",
 });
 
 api.interceptors.request.use(
